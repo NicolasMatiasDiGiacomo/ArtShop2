bd28f38fef952d4775af72f104eff02245e55415
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-06-28 16:35:05 -0300

    Editar Artista

--- ArtShop.WebSite/ArtShop.WebSite/Controllers/ArtistController.cs
+++ ArtShop.WebSite/ArtShop.WebSite/Controllers/ArtistController.cs
@@ -4,6 +4,7 @@ using OdeToFood.WebSite.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -42,6 +43,42 @@ namespace ArtShop.WebSite.Controllers
             try
             {
                 db.Create(artist);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.MessageDanger = ex.Message;
+                return View(artist);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var artist = db.GetById(id.Value);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Artist artist)
+        {
+            this.CheckAuditPattern(artist, true);
+            var list = db.ValidateModel(artist);
+            if (ModelIsInvalid(list))
+            {
+                return View(artist);
+            }
+            try
+            {
+                db.Update(artist);
                 return RedirectToAction("Index");
             }
             catch (Exception ex)
